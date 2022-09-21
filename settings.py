653a0129450087feecbd688d7269f463323ef19d
Динара Фатехова <dfatekhova@gmail.com>
2022-09-21 15:43:51 +0200

    HW 20.0

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
+import os
+
+
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'MY SECRET KEY'
+    SECRET_KEY = os.getenv('SECRET_KEY')
